6420853855556eb420356610d34749faddbef4aa
zhyarali <zhyaralimahmud@gmail.com>
2022-04-10 01:25:41 +0300

    updated! updated!

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,11 @@ export default {
   css: [
   ],
 
+  buildDir: 'dist',
   target: 'static',
+  generate: {
+    fallback: true,
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
